bdbfdc9e7b3aa5d484e09058c8c06993a94123d1
Michael Greco <michael.greco07@gmail.com>
2020-04-12 15:39:40 -0400

    Video progress bar & timestamp

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
-// Play and Pause Video
+// Play & pause video
 function toggleVideoStatus() {
   if (video.paused) {
     video.play();
@@ -13,31 +13,46 @@ function toggleVideoStatus() {
   }
 }
 
+// update play/pause icon
 function updatePlayIcon() {
   if (video.paused) {
-    play.innerHTML = '<iclass="fa fa-play fa-2x"></i>';
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
   } else {
-    play.innerHTML = '<iclass="fa fa-pause fa-2x"></i>';
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
   }
 }
 
-// Update progress and timestamp
+// Update progress & timestamp
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
-// Stop Video
+// Stop video
 function stopVideo() {
   video.currentTime = 0;
   video.pause();
 }
 
-// Event Listeners
+// Event listeners
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
